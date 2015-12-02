795f9921fff031a70335acd30628978d3eecc2a1
Richard Diamond <wichard@vitalitystudios.com>
2015-12-02 17:44:53 -0600

    Move bindings from servo/skia to this repo.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,55 @@
-#[test]
-fn it_works() {
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+extern crate euclid;
+extern crate gleam;
+extern crate libc;
+
+#[cfg(target_os="macos")]
+extern crate cgl;
+#[cfg(target_os="macos")]
+extern crate io_surface;
+
+#[cfg(target_os="linux")]
+extern crate x11;
+#[cfg(target_os="linux")]
+extern crate glx;
+
+#[cfg(target_os="android")]
+extern crate egl;
+
+#[cfg(any(target_os="linux", target_os="android", target_os="windows"))]
+extern crate freetype_sys;
+
+#[cfg(any(target_os="windows"))]
+extern crate fontconfig;
+
+extern crate skia_sys;
+
+mod skia {
+    pub use skia_sys::*;
 }
+
+pub mod gl_context;
+pub mod gl_rasterization_context;
+
+#[cfg(target_os="linux")]
+pub mod gl_context_glx;
+#[cfg(target_os="linux")]
+pub mod gl_rasterization_context_glx;
+
+#[cfg(target_os="macos")]
+pub mod gl_context_cgl;
+#[cfg(target_os="macos")]
+pub mod gl_rasterization_context_cgl;
+
+#[cfg(target_os="android")]
+pub mod gl_context_android;
+#[cfg(target_os="android")]
+pub mod gl_rasterization_context_android;
+
+#[cfg(target_os="windows")]
+pub mod gl_context_wgl;
+#[cfg(target_os="windows")]
+pub mod gl_rasterization_context_wgl;
